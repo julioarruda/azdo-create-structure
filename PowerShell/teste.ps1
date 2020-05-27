70a430d118a0f45ea8255868b03fce5c8f7f8aae
Automated Process <automated@gft.com>
2020-05-27 17:50:51 +0000

    remove test.ps1

--- PowerShell/teste.ps1
+++
@@ -1,7 +0,0 @@
-$currentDir = $PSScriptRoot
-$rootFolder = $PSScriptRoot.Replace('/PowerShell','').Replace('\PowerShell','')
-$gitignore = "$($rootFolder)\.gitignore"
-
-Write-Host $currentDir
-Write-Host $rootFolder
-Write-Host $gitignore
