87791db4f5cab9d7e2efff973ee2380712a54e62
Julio Arruda <julio.arruda@outlook.com>
2020-05-27 17:01:17 +0000

    commit inicial

---
+++ PowerShell/Create_AzDOStructure.ps1
@@ -0,0 +1,235 @@
+Param
+(
+    [string]$Organization,
+    [string]$Project,
+    [string]$RepoName,
+    [ValidateSet("DotNetCoreMVC")]
+    [string]$ProjectType,
+    [string]$BuildConfiguration,
+    [string]$SonarCloudAccount,
+    [string]$SonarCloudOrganization,
+    [string]$SonarProjectKey,
+    [string]$SonarProjectName,
+    [string]$ReviewersDefault,
+	[string]$ReviewersTeam,
+    [string]$PAT
+)
+
+
+Function Check-RepositoryExist ($repository) {
+    
+        try{
+            $ErrorActionPreference = 'Stop'
+           $teste= az repos show --r $repository  #| ConvertFrom-Json# -ErrorAction:SilentlyContinue
+           
+            return $true
+     
+        }
+        catch [System.Management.Automation.RemoteException] {
+            Write-Host "Exception"
+             return $false
+        }
+        catch{
+            Write-Host "erro generico"
+        }
+    }
+
+Function Add-GitRepo
+{
+    Param
+    (
+        [string] $repoName
+    )
+    Write-Host '===Criando repositório no Azure Repos'
+  # if(Check-RepositoryExist $repoName)
+  # {
+   #    Write-Host "Repositório Já Existe, o processo será interrompido"
+  #     break
+  # }
+ #  else {
+    
+    
+    $createRepo = az repos create --name $repoName | ConvertFrom-Json
+
+    Write-Host '======Remote URL: ' $createRepo.sshUrl
+    Write-Host '======Repo ID: ' $createRepo.id
+    return $createRepo
+   #}
+
+   # $createRepo = az repos create --name $repoName | ConvertFrom-Json
+
+   # Write-Host '======Remote URL: ' $createRepo.sshUrl
+   # Write-Host '======Repo ID: ' $createRepo.id
+    return $createRepo
+}
+
+Function Set-GitPush
+{
+    Param
+    (
+        [string] $remoteUrl
+    )
+    Write-Host '===Push inicial de aplicaçãoo exemplo'
+    git add .
+    git commit -m 'Commit Inicial'
+    git remote add origin $remoteUrl
+    git push --set-upstream origin master --quiet
+}
+
+Function Set-Gitignore 
+{
+    Copy-Item -Path $gitignore -Destination $currentDir\$RepoName'\.gitignore'
+    git add .
+    git commit -m 'Primeiro commit no novo repositório e Inclusão do gitignore'
+}
+
+
+Function Add-ProjectTypeSolution
+{
+    Param
+    (
+        [string] $ProjectType,
+        [string] $RepoName
+    )
+
+    Write-Host '===Criação do tipo de aplicação' $ProjectType
+
+
+    switch ( $ProjectType )
+    {
+        'DotNetCoreMVC' 
+        {
+            dotnet new sln --name $RepoName
+            dotnet new mvc --name $RepoName
+            dotnet sln add .\$RepoName\$RepoName.csproj
+
+        }
+    }
+}
+
+Function Set-BranchPolicy
+{
+    Param
+    (
+        [string] $repoId,
+        [string] $pipelineId,
+        [string] $pipeline,
+        [string] $ReviewersTeam,
+        [string] $ReviewersDefault
+    )
+    Write-Host '===Estabelecendo as policies da branch master'
+
+    Write-Host '======Policy: Require a minimum number of reviewers'
+    $policyApproverCount = az repos policy approver-count create --allow-downvotes false --blocking true --branch master --creator-vote-counts false --enabled true --minimum-approver-count 2 --repository-id $repoId --reset-on-source-push true | ConvertFrom-Json
+    Write-Host '======' $policyApproverCount.createdDate
+    
+    Write-Host '======Policy: Checked for linked work items'
+    $policyWorkItemLinking = az repos policy work-item-linking create --blocking true --branch master --enabled true --repository-id $repoId | ConvertFrom-Json
+    Write-Host '======' $policyWorkItemLinking.createdDate 
+
+    Write-Host '======Policy: Checked for comment resolution'
+    $policyCommentRequired = az repos policy comment-required create --blocking true --branch master --enabled true --repository-id $repoId | ConvertFrom-Json
+    Write-Host '======' $policyCommentRequired.createdDate
+
+    Write-Host '======Policy: Automatically include code reviewers'
+    $policyRequiredReviewerTeam = az repos policy required-reviewer create --blocking true --branch master --enabled true --message 'Including Code Reviewers' --repository-id $repoId --required-reviewer-ids $ReviewersTeam  | ConvertFrom-Json
+    Write-Host '======' $policyRequiredReviewerTeam.createdDate
+    $policyRequiredReviewerDevOps = az repos policy required-reviewer create --blocking true --branch master --enabled true --message 'Including Default Reviewers' --repository-id $repoId --required-reviewer-ids $ReviewersDefault  | ConvertFrom-Json
+    Write-Host '======' $policyRequiredReviewerDevOps.createdDate
+
+    Write-Host '======Policy: Build Validation'
+    $policyBuildValidation = az repos policy build create --blocking true --branch master --build-definition-id $pipelineId --display-name $pipeline --enabled true --manual-queue-only false --queue-on-source-update-only false --repository-id $repoId --valid-duration 0 | ConvertFrom-Json
+    Write-Host '======' $policyBuildValidation.createdDate
+}
+
+Function Add-Pipelines 
+{
+    Param
+    (
+        [string] $pipelinePrincipal,
+        [string] $remoteUrl
+    )
+    Write-Host '===Criação de Pipeline Definitions'
+    Write-Host '======Criação da Pipeline Definition Principal' $pipelinePrincipal
+    $createPipelinePrincipal = az pipelines create --name $pipelinePrincipal --branch master --description 'Pipeline Principal' --repository $remoteUrl --repository-type 'tfsgit' --skip-first-run true --yaml-path '\esteiras\build-principal.yml' | ConvertFrom-Json
+    Write-Host '======' $createPipelinePrincipal.createdDate
+    Write-Host '======Enfileiramento da Pipeline Principal' $pipelinePrincipal
+    $queuePipelinePrincipal = az pipelines build queue --definition-id $createPipelinePrincipal.id | ConvertFrom-Json
+    Write-Host '======' $queuePipelinePrincipal.buildNumber
+
+    
+    return $createPipelinePrincipal.id
+}
+
+
+#$Organization = 'https://dev.azure.com/demoserievideo1/'
+#$Project = 'NovoDemo'
+#$ReviewersDefault = '[demoserievideo1]\teste'
+#$ProjectType = 'DotNetCoreMVC'
+
+$urlConcat = "https://dev.azure.com/$($Organization)"
+
+git config --global user.name "Automated Process"
+git config --global user.email "automated@gft.com"
+Set-Location $PSScriptRoot
+Write-Host $PSScriptRoot
+
+$env:AZURE_DEVOPS_EXT_PAT = $PAT
+Write-Host $urlConcat
+#az devops login --org $urlConcat
+echo $PAT | az devops login --org $urlConcat
+
+Write-Host '===Configurando conexão com a organization e o Team Project'
+az devops configure --defaults organization=$urlConcat project=$Project
+
+#cria repo
+$createRepo = Add-GitRepo -repoName $RepoName
+$currentDir = $PSScriptRoot
+$rootFolder = $PSScriptRoot.Replace('\PowerShell','')
+$gitignore = "$($rootFolder)\.gitignore"
+
+New-Item -Path $RepoName -ItemType Directory
+Set-Location $currentDir\$RepoName
+
+
+
+$FolderExists = Get-ChildItem $rootFolder -Filter $ProjectType -Recurse -Directory 
+
+
+if($ProjectType -eq "DotNetCoreMVC"){
+    $FolderProjectType = $FolderExists.FullName
+    
+}
+
+#inicializa git repo
+git init
+
+#Inclui Gitignore
+Set-Gitignore
+
+New-Item -Path 'esteiras' -ItemType Directory
+Copy-Item -Path "$FolderProjectType\*.yml" -Destination $currentDir\$RepoName'\esteiras' -Recurse
+
+
+(Get-Content $currentDir\$RepoName'\esteiras\variables.yml') | Foreach-Object {
+    $_ -replace '__BuildConfiguration__', $BuildConfiguration `
+        -replace '__SonarCloudAccount__', $SonarCloudAccount `
+        -replace '__SonarCloudOrganization__', $SonarCloudOrganizatio `
+        -replace '__SonarProjectKey__', $SonarProjectKey `
+        -replace '__SonarProjectName__', $SonarProjectName `
+    } | Set-Content $currentDir\$RepoName'\esteiras\variables.yml'
+
+
+
+
+Add-ProjectTypeSolution -ProjectType $ProjectType -RepoName $RepoName
+
+#push no repositorio
+Set-GitPush -remoteUrl $createRepo.sshUrl
+
+#inclui o pipeline
+$pipelinePrincipal = $RepoName 
+$pipelineId = Add-Pipelines -pipelinePrincipal $pipelinePrincipal -remoteUrl $createRepo.remoteUrl
+
+#aplica politicas de branch
+Set-BranchPolicy -repoId $createRepo.id -ReviewersTeam $ReviewersTeam -ReviewersDefault $ReviewersDefault -pipelineId $pipelineId -pipeline $pipelinePrincipal
--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# azdo-create-structure
-Project used to automate create your repository with .Net Core default application and Pipeline
+# Create Azure DevOps Repo
+
+Using this script, you will be able to create Azure DevOps repos, create a starter .Net Core Application, CI Pipeline and apply default branch policies on this repo.
+
+#Pre Requisites
+
+1. [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest)
+2. [Azure DevOps CLI](https://docs.microsoft.com/en-us/cli/azure/ext/azure-devops/devops?view=azure-cli-latest)
+3. [GIT](https://git-scm.com/download/win)
+4. [SSH Certificate](https://dev.to/bdbch/setting-up-ssh-and-git-on-windows-10-2khk)
+
+# Azure CLI
+To run this script, you need install Azure CLI. To install this one, you need to run this script in your PowerShell with administrator permission.
+```
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+```
+Using this script, you will install a latast version of CLI in your environment.
+
+# Azure DevOps CLI
+This script, use commands from Azure DevOps CLI, and this is a part of Azure CLI installed previously. To install this part of CLI, you need to run this script in your PowerShell with administrator permission.
+```
+az extension add --name azure-devops
+```
+
+# SSH
+You need an ssh certificate to connect in your new Git repo on Azure DevOps created using this script. To generate your ssh key, you can run this command in your PowerShell:
+```
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+```
+
+Now, you need to get the value from your Public Key generated before, and save this on Azure DevOps. To save this in Azure DevOps, 
+
+![imagem](img/ssh1.png)
+![imagem](img/ssh2.png)
+
+# Personal Access Token
+You need to generate a new Personal Access Token (PAT) with permissions to create your repository. You will use this PAT in this script to connect in Azure DevOps, and create your repo, and apply the policies on master branch.
+![imagem](img/pat1.png)
+![imagem](img/pat2.png)
+
+# Script
+To run this script, you need to put this parameters:
+
+    1. $Organization - Your Azure DevOps Account, like: demoserievideo1
+    2. $Project - Name of destination project, like: CostumerProject
+    3. $RepoName - Name of your new Repository
+    4. $ProjectType - Type of Your project, for now, you can to create only DotNetCoreMVC
+    5. $BuildConfigurationv - Build Configuration for your application (debug/release)
+    6. $SonarCloudAccount - Name of your Sonar Cloud Account configured on Azure DevOps
+    7. $SonarCloudOrganization - name of your organization on Sonar Cloud
+    8. $SonarProjectKey - Sonar Project Key
+    9. $SonarProjectName - Sonar Project Name
+    10. $ReviewersDefault - Name of Default Reviewers Group on Azure DevOps, like: [demoserievideo1]\approvers
+	11. $ReviewersTeam - Name of Second Reviewers Group on Azure DevOps, like: [demoserievideo1]\OtherApprovers
+    12. $PAT - Azure DevOps Personal Access Token
+
+# Execution
+This script will be execute this steps:
+
+1. Connect on Azure DevOps
+2. Check if this repository exists
+3. Create this repository
+4. Clone in local machine
+5. Including .gitignore on repository
+6. Create a starter project
+7. Including pipeline files
+6. Commit this change
+7. Pull this change to remote
+8. Configure the pipeline
+8. Apply branch policies.
+
+You can see the policies on Azure DevOps, in Branch options in repository. The polices enabled with this scripts are:
+1. Minimun Reviewer Number
+2. Check Linked Work Item
+3. Check for Comment Resolution
+4. Automatically include code reviewers
+
+![imagem](img/police1.png)
+![imagem](img/police2.png)
+
+# Run
+To run this, you need to clone this repo, and run this script like this.
+```
+.\Create_AzDOStructure-GFT.ps1 -Organization Organization -Project Project -RepoName Repo -ProjectType DotNetCoreMVC -BuildConfiguration release -SonarCloudAccount sonar -SonarCloudOrganization sonar -SonarProjectKey key -SonarProjectName name -ReviewersDefault [project]\reviewer -ReviewersTeam [project]\reviewer -PAT skdasjdklasjdklasjkdjaklsdjaksjdklas  
+```
